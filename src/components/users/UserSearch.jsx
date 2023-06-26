4def2cabb6e19174c31afefbc14b6d5ed0076218
Uzair Oomer <uzair.oomer@jdplc.com>
2023-06-26 17:06:22 +0100

    Pass get user and getuserrepos to actions file and call in components

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -7,7 +7,7 @@ import { useState, useContext } from "react"
 function UserSearch() {
     const [text, setText] = useState('')
 
-    const { users, dispatch, clearUsers } = useContext(GithubContext)
+    const { users, dispatch } = useContext(GithubContext)
 
     const {setAlert} = useContext(AlertContext)
 
@@ -42,7 +42,7 @@ function UserSearch() {
             </div>
             {users.length > 0 && (
             <div>
-                <button onClick={clearUsers} className="btn btn-ghost btn-lg">Clear</button>
+                <button onClick={() => dispatch({type: 'CLEAR'})} className="btn btn-ghost btn-lg">Clear</button>
             </div>) }
         </div>
     )
