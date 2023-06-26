bcad229bf7fcd894f5c91abd8c337f127751c6df
Uzair Oomer <uzair.oomer@jdplc.com>
2023-06-26 17:24:07 +0100

    code refactoring part 2

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,5 +1,13 @@
+import axios from 'axios'
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+const github = axios.create({
+    baseURL: GITHUB_URL,
+    headers: {
+        Authorization: `token${GITHUB_TOKEN}`,
+    }
+})
 
 // Get search results
 export const searchUsers = async (text) => {
@@ -7,15 +15,8 @@ export const searchUsers = async (text) => {
     const params = new URLSearchParams({
         q: text
     })
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-        headers: {
-            Authorization: `token${GITHUB_TOKEN}`
-        }
-    })
-
-    const {items} = await response.json()
-
-    return items
+    
+    const response = await github.get(`/search/users`)
 }
 
 // Get single user
